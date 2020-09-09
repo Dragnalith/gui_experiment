1b078c8eb43bee1dbb0d136984dff82197b5efa3
Marc Delorme <delormemarco@gmail.com>
2020-09-09 22:26:54 +0900

    Experiment with custom widget

--- LOG.md
+++ LOG.md
@@ -3,3 +3,13 @@
 - I have added a cmake function to copy Qt dll next to build executable
 - It is possible to setup debug in Qt sources by adding Qt source folder to debug file source property of the solution
 - By using the windeployqt.exe as post build step you can automatically copy necessary dependency next to the built executable
+
+# 2020/09/09
+- Q_D is a macro to get the private pimpl pointer of a Qt class
+- Q_Q is a macro to get the public class from a pimpl
+- When an area is invalidate by the system, it generate an exposeEvent to the corresponding window. The window knows the root widget and use a QWidgetRepaintManager to recursively send paint event to the widget tree. It recursively call QWidgetPrivate::drawWidget which call "paintEvent" method.
+- Because QWidget inherit from QPaintDevice, it is possible to create a QPainter using a QWidget
+- Qt Widget style system seems to have baked a fixed number of ui element (menu, button, etc). So you can draw you custom widget using the baked ui element as primitive.
+- [ ] Create custom widget with rectangle and position them  with custom layout
+? Why MyRectangle is cropped to a region smaller than its parent window size
+- [ ] Animate widget rendering
--- source/QtWidgetApp/main.cpp
+++ source/QtWidgetApp/main.cpp
@@ -1,10 +1,46 @@
 #include <QApplication>
 #include <QMainWindow>
+#include <QPushButton>
+#include <QPainter>
+
+class MyRectangle : public QWidget {
+public:  
+    MyRectangle(const QColor& color, QWidget* parent = nullptr)
+        : QWidget(parent)
+        , m_color(color)
+    {
+        setSizePolicy(QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum));
+    }
+
+    virtual QSize sizeHint() const override {
+        return QSize(200, 150);
+    }
+
+    virtual void paintEvent(QPaintEvent* ev) override {
+        QPainter painter(this);
+        painter.setRenderHint(QPainter::Antialiasing, true);
+        painter.setPen(QPen(m_color, 4));
+        auto g = geometry();
+        painter.drawRect(g);
+    }
+private:
+    QColor m_color;
+};
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
-    QMainWindow window;
+
+    QWidget window;
+    window.setWindowTitle("Window");
+    window.setFixedSize(800, 600);
+    QPushButton* button = new QPushButton(&window);
+    button->setText("Press Me!");
+    MyRectangle* rect = new MyRectangle(Qt::GlobalColor::green, &window);
+    auto g = rect->geometry();
+    g.setX(g.x() + 10);
+    rect->setGeometry(g);
+
     window.show();
 
     return app.exec();
